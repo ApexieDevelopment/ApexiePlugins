abf004e023027d39e044c07ef12ad1cf1a3858a1
ItzLightyHD <itzlightyhd@gmail.com>
2021-03-19 17:42:34 +0100

    LobbyPE - Added the "/alert" command

--- LobbyPE/src/main/java/org/apexie/lobbype/AlertCommand.java
+++ LobbyPE/src/main/java/org/apexie/lobbype/AlertCommand.java
@@ -1,19 +1,14 @@
 package org.apexie.lobbype;
 
-import com.nukkitx.protocol.bedrock.data.command.CommandData;
-import com.nukkitx.protocol.bedrock.data.command.CommandParamData;
-import com.nukkitx.protocol.bedrock.data.command.CommandParamType;
+import dev.waterdog.ProxyServer;
 import dev.waterdog.command.Command;
 import dev.waterdog.command.CommandSender;
 import dev.waterdog.command.CommandSettings;
 import dev.waterdog.utils.Configuration;
 
-import java.util.Collections;
-
 public class AlertCommand extends Command {
 
     public AlertCommand() {
-
         super("alert", CommandSettings.builder()
                 .setDescription("Broadcasts a message to all online players")
                 .setPermission("alert.command")
@@ -25,22 +20,12 @@ public class AlertCommand extends Command {
     public boolean onExecute(CommandSender commandSender, String s, String[] strings) {
         Configuration configuration = LobbyPE.getInstance().getConfig();
         String alertUsage = configuration.getString("messages.alertUsage");
-        String alertFormat = configuration.getString("messages.alert-format");
+        String alertFormat = configuration.getString("alert-format");
         if(strings.length == 0) {
             commandSender.sendMessage(alertUsage.replace("%alert.usage%", getUsageMessage()));
         } else {
-            LobbyPE.getInstance().getProxy().getPlayers().forEach((uuid, proxiedPlayer) -> {
-                proxiedPlayer.sendMessage(alertFormat.replace("%alert.message%", String.join(" ", strings)));
-            });
+            ProxyServer.getInstance().getPlayers().values().forEach(p -> p.sendMessage(alertFormat.replace("%alert.message%", String.join(" ", strings))));
         }
         return true;
-    }
-
-    @Override
-    public CommandData craftNetwork() {
-        CommandParamData[][] parameterData = new CommandParamData[][]{{
-                new CommandParamData("message", false, null, CommandParamType.TEXT, null, Collections.emptyList())
-        }};
-        return new CommandData(this.getName(), this.getDescription(), Collections.emptyList(), (byte) 0, null, parameterData);
     }
 }
